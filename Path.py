3013d7f374d9ff9f8a966c3147b2a8eb61d98c1e
Marc Dolores <157381837+nyammmm@users.noreply.github.com>
2025-05-12 18:59:39 +0800

    Update Path.py

--- Path.py
+++ Path.py
@@ -74,32 +74,40 @@ distance_matrix = compute_distance_matrix(coordinates)
 
 # Solve TSP with OR-Tools
 def solve_tsp(distance_matrix):
-    manager = pywrapcp.RoutingIndexManager(len(distance_matrix), 1, 0)
+    from ortools.constraint_solver import routing_enums_pb2
+    from ortools.constraint_solver import pywrapcp
+
+    manager = pywrapcp.RoutingIndexManager(len(distance_matrix), 1, 0)  # start and end at node 0
     routing = pywrapcp.RoutingModel(manager)
 
-    def distance_callback(from_idx, to_idx):
-        return distance_matrix[manager.IndexToNode(from_idx)][manager.IndexToNode(to_idx)]
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return int(distance_matrix[from_node][to_node] * 1000)  # convert km to meters
 
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    search_params = pywrapcp.DefaultRoutingSearchParameters()
-    search_params.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    # Force round trip
+    routing.SetStartEnd(0, 0)
 
-    solution = routing.SolveWithParameters(search_params)
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    solution = routing.SolveWithParameters(search_parameters)
+
     if solution:
         index = routing.Start(0)
         route = []
-        total_dist = 0
         while not routing.IsEnd(index):
-            route.append(manager.IndexToNode(index))  # End node
-            route.append(0)  # Explicitly return to start (node 0)
-            prev_index = index
+            route.append(manager.IndexToNode(index))
             index = solution.Value(routing.NextVar(index))
-            total_dist += routing.GetArcCostForVehicle(prev_index, index, 0)
-        route.append(manager.IndexToNode(index))  # return to start
-        return route, total_dist / 1000  # in km
-    return None, 0
+        route.append(manager.IndexToNode(index))  # return to depot
+        return route
+    else:
+        return None
+
 
 route_indices, total_distance = solve_tsp(distance_matrix)
 
@@ -107,7 +115,7 @@ if route_indices is None:
     st.error("Could not solve route.")
     st.stop()
 
-ordered_addresses = [addresses[i] for i in route_indices]
+ordered_addresses = [addresses[i] for i in route]
 
 st.subheader("📦 Optimal Route")
 for i, addr in enumerate(ordered_addresses):
